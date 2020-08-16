3e71c94cc41d421ed3d9746795abef10ba536da9
katharina.sachs <katharina.sachs@seerene.com>
2020-08-16 20:52:23 +0200

    add auth option value

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import GAuth from 'vue-google-oauth2'
 
 const gauthOption = {
-  clientId: process.env['GOOGLE_CLIENT_ID'],
+  clientId: '516982342717-6ri9qn82ucchr13ftdec279bm2if82n0.apps.googleusercontent.com',
   scope: 'profile email',
   prompt: 'select_account'
 }
