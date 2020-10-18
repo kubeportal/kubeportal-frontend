160a5ac6e3d1235c8679b8426e3b1b727f2ea79c
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2020-10-18 22:50:42 +0200

    added form to login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,28 @@
 <template>
     <b-card bg-variant="light" class="mt-16 logincard">
-      <b-card-header>
-        <v-icon class="icon" left>mdi-login-variant</v-icon>
-        Kubeportal
-      </b-card-header>
-      <b-card-body>
-        <v-alert class="alert" dense outlined type="error" v-if="is_authenticated === 'false'">Login Failed.</v-alert>
-        <b-card-text>
-          <v-text-field label="user name" v-model="username" required></v-text-field>
-          <v-text-field type="password" v-model="password" label="password" required></v-text-field>
-        </b-card-text>
-        <div class="row">
-          <b-button class="signin" @click="login">Sign In</b-button>
-        </div>
-        <div class="row"><p class="my-4 text">or</p></div>
-        <div class="row">
-          <b-button class="signin" @click="signInWithGoogle">
-            <v-icon  white small left>mdi-google</v-icon>
-            Continue with Google
-          </b-button>
-        </div>
-      </b-card-body>
+      <b-form @submit.prevent="login">
+        <b-card-header>
+          <v-icon class="icon" left>mdi-login-variant</v-icon>
+          Kubeportal
+        </b-card-header>
+        <b-card-body>
+          <v-alert class="alert" dense outlined type="error" v-if="is_authenticated === 'false'">Login Failed.</v-alert>
+          <b-card-text>
+            <v-text-field label="user name" v-model="username" required></v-text-field>
+            <v-text-field type="password" v-model="password" label="password" required></v-text-field>
+          </b-card-text>
+          <div class="row">
+            <b-button type="submit" class="signin">Sign In</b-button>
+          </div>
+          <div class="row"><p class="my-4 text">or</p></div>
+          <div class="row">
+            <b-button class="signin" @click="signInWithGoogle">
+              <v-icon  white small left>mdi-google</v-icon>
+              Continue with Google
+            </b-button>
+          </div>
+        </b-card-body>
+      </b-form>
     </b-card>
 </template>
 
@@ -41,7 +43,7 @@ export default {
     async login () {
       const request_body = { username: this.username, password: this.password }
       const response = await this.$store.dispatch('users/post_login_data', request_body)
-      console.log(response);
+      console.log(response)
 
       await this.handle_login_response(response)
     },
