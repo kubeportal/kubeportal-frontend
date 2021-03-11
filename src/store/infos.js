cb769430745b824a9b29d03f724527f1508be5c8
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2021-03-11 15:49:03 +0100

    fix cluster name

--- src/store/infos.js
+++ src/store/infos.js
@@ -6,24 +6,13 @@ const infos = {
     state: {
       infos_url: '',
       cluster_name: '',
-      cluster_request_info: [
-        'portal_user_count',
-        'portal_version',
-        'k8s_version',
-        'k8s_node_count',
-        'k8s_cpu_count',
-        'k8s_mem_sum',
-        'k8s_pod_count',
-        'k8s_volume_count',
-        'k8s_apiserver_url',
-        'k8s_cluster_name'
-      ],
+      cluster_links: [],
       cluster_info: []
     },
 
     getters: {
       get_infos_url (state) { return state.infos_url },
-      get_cluster_request_info (state) { return state.cluster_request_info },
+      get_cluster_links (state) { return state.cluster_links },
       get_cluster_info (state) { return state.cluster_info },
       get_infos (state) { return state.infos },
       get_cluster_name (state) { return state.cluster_name }
@@ -31,6 +20,7 @@ const infos = {
 
     mutations: {
       set_infos_url (state, infos_url) { state.infos_url = infos_url },
+      set_cluster_links (state, links) { state.cluster_links = links },
       set_cluster_info (state, info) { state.cluster_info = info },
       push_cluster_info (state, info) { state.cluster_info.push(info) },
       set_info (state, name, info) { state.infos[name] = info },
@@ -41,6 +31,7 @@ const infos = {
       async request_cluster_infos (context) {
         const response = await backend.get(context.state.infos_url)
         const links = response.data['links']
+        context.commit('set_cluster_links', links)
         for (const key in links) {
           backend.get(links[key]).then(info_response => {
             context.commit('push_cluster_info', info_response.data)
@@ -48,9 +39,10 @@ const infos = {
         }
       },
       async request_cluster_name (context) {
-        let response = await backend.get(`/cluster/k8s_cluster_name/`)
+        const cluster_links = context.getters['get_cluster_links']
+        let response = await backend.get(cluster_links['k8s_cluster_name'])
         console.log('CLUSTER NAME', response)
-        context.commit('set_cluster_name', response.data.k8s_cluster_name)
+        context.commit('set_cluster_name', response.data['k8s_cluster_name'])
       }
     }
   }
