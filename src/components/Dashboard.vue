599948eabd36f7bf6a9d532712438776352fb7b5
s77518 <katharina.sachs@beuth-hochschule.de>
2021-03-23 19:58:15 +0100

    #72: mobile version

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,59 +1,79 @@
 <template>
   <v-card>
-    <v-tabs vertical class="sidenav" dark active-class="activeTab">
-      <v-img class="img"
-        src="../assets/mountain.jpeg"
-        gradient="to bottom left, rgba(18,18,18, .8), rgba(18, 18, 18, .3)">
-        <div class="logo text-center" @click="go_to_dashboard">
-          <v-row align="center" >
-            <v-col>
-              <v-icon class="icon">mdi-view-dashboard-variant</v-icon>
-            </v-col>
-            <v-col>
-              <div class="title"><small>{{cluster_branding}}</small></div>
-            </v-col>
-          </v-row>
-        </div>
-        <v-container>
-          <hr />
-        </v-container>
-        <v-tab v-for="tab in filtered_tabs" :key="tab.name">
-          <v-row>
-            <v-col sm="4">
-              <v-icon class="icon">{{ tab.icon }}</v-icon>
-            </v-col>
-            <v-col sm="6">
-              <div class="title">
-                <small>{{ tab.name }}</small>
-              </div>
-            </v-col>
-          </v-row>
-        </v-tab>
-      </v-img>
-        <v-tab-item
-          v-for="(tab, index) in filtered_tabs"
-          :key="tab.name + index"
-          class="items">
-          <Node :tab="tab" />
-        </v-tab-item>
-    </v-tabs>
+    <hide-at :breakpoints="{small: 600, medium: 1000, large: 1600}"  breakpoint="small">
+      <v-tabs vertical class="sidenav" dark active-class="activeTab">
+        <v-img class="vertical-img" src="../assets/mountain.jpg"
+               gradient="to bottom left, rgba(18,18,18, .8), rgba(18, 18, 18, .3)">
+          <div class="logo text-center" @click="go_to_dashboard">
+            <v-row align="center" >
+              <v-col>
+                <v-icon class="vertical-icon">mdi-view-dashboard-variant</v-icon>
+              </v-col>
+              <v-col>
+                <div class="title"><small>{{cluster_name}}</small></div>
+              </v-col>
+            </v-row>
+          </div>
+          <v-container>
+            <hr />
+          </v-container>
+          <v-tab v-for="tab in filtered_tabs" :key="tab.name">
+            <v-row>
+              <v-col sm="4">
+                <v-icon class="vertical-icon">{{ tab.icon }}</v-icon>
+              </v-col>
+              <v-col sm="6">
+                <div class="title">
+                  <small>{{ tab.name }}</small>
+                </div>
+              </v-col>
+            </v-row>
+          </v-tab>
+        </v-img>
+          <v-tab-item v-for="(tab, index) in filtered_tabs" :key="tab.name + index" class="items">
+            <Node :tab="tab" />
+          </v-tab-item>
+      </v-tabs>
+    </hide-at>
+
+    <show-at :breakpoints="{small: 600, medium: 1000, large: 1600}"  breakpoint="small">
+        <v-tabs grow centered icons-and-text show-arrows dark active-class="activeTab">
+          <v-img src="../assets/mountain.jpg" gradient="to bottom left, rgba(18,18,18, .8), rgba(18, 18, 18, .3)">
+            <v-row>
+              <v-tab @click="go_to_dashboard">
+                <v-icon large class="mt-4 top-icon">mdi-view-dashboard-variant</v-icon>
+              </v-tab>
+              <v-tab v-for="tab in filtered_tabs" :key="tab.name" class="d-inline-block">
+                <v-icon large class="mt-4 top-icon">{{ tab.icon }}</v-icon>
+              </v-tab>
+            </v-row>
+          </v-img>
+          <v-tab-item v-for="(tab, index) in filtered_tabs" :key="tab.name + index">
+            <Node :tab="tab" />
+          </v-tab-item>
+        </v-tabs>
+    </show-at>
   </v-card>
 </template>
 
 <script>
-import { showAt } from 'vue-breakpoints'
+import { showAt, hideAt } from 'vue-breakpoints'
 import Node from './Node'
 
 export default {
   name: 'Dashboard',
+  components: { showAt, hideAt, Node },
+
   methods: {
     go_to_dashboard () {
       if (this.$route.name !== 'Kubeportal') {
         this.$router.push({ name: 'Kubeportal' })
       }
+    },
+    request_cluster_name () {
+      this.$store.dispatch('infos/request_cluster_name')
     }
   },
-  components: { showAt, Node },
   props: ['tabs'],
   computed: {
     filtered_tabs () {
@@ -65,24 +85,28 @@ export default {
   },
   created () {
     this.$vuetify.theme.dark = this.$store.getters['users/get_dark_mode']
+    if (this.cluster_name === '') this.request_cluster_name()
   }
 }
 </script>
 
 <style scoped lang="scss">
-.icon {
+.vertical-icon {
   color: floralwhite;
 }
+.top-icon {
+  width: 14px
+}
+
 .sidenav {
   height: 100vh;
   position: absolute;
   left: -2px;
   top: -2px;
-  overflow: auto;
 }
 
 .activeTab {
-  background: linear-gradient(to top right, #b3b3b3, #4d4d4d)
+  background: linear-gradient(to top right, #b3b3b3, #4d4d4d);
 }
 
 .logo {
@@ -103,9 +127,10 @@ hr {
 .items {
   max-height: 100vh;
 }
-.img {
+.vertical-img {
   width: 12vw;
   height: 100vh;
   min-width: 200px;
 }
+
 </style>
