78a0458af263334ec45591662331e53f10bf54d9
s77518 <katharina.sachs@beuth-hochschule.de>
2021-03-25 21:02:52 +0100

    #105: set namespace information in node to avoid redundance

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -2,6 +2,7 @@
   <div>
     <TopBar :title="tab.name"/>
     <v-card>
+      <v-card-text> current namespace: {{ namespace }}</v-card-text>
       <component :is="tab.component"/>
     </v-card>
   </div>
@@ -12,7 +13,12 @@ import TopBar from '@/components/TopBar'
 export default {
   name: 'Node',
   props: ['tab'],
-  components: { TopBar }
+  components: { TopBar },
+  computed: {
+  namespace () {
+    return this.$store.getters['users/get_user']['namespace_names'].join(', ')
+  },
+}
 }
 </script>
 
