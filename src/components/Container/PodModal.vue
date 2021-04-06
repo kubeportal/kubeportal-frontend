c52c8380ff1e565844bd08a159876c50c28353c7
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2021-04-06 10:34:46 +0200

    dynamic containers size

--- src/components/Container/PodModal.vue
+++ src/components/Container/PodModal.vue
@@ -4,7 +4,24 @@
       <v-card-title> Create Pod </v-card-title>
       <v-card-text>
         <v-form @submit="post_pod">
-          <v-text-field v-model="name" label="Name" required> </v-text-field>
+          <v-text-field v-model="name" label="Pod Name" required> </v-text-field>
+          <v-row v-for="(container, index) in containers" :key="index">
+            <v-col md="5">
+              <v-text-field v-model="container.name" label="Container Name" />
+            </v-col>
+            <v-col md="5">
+              <v-text-field v-model="container.image" label="Container Image" />
+            </v-col>
+            <v-col md="2">
+              <v-btn
+                icon
+                large
+                @click="containers.push({name:'', image:''})"
+              >
+              <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+            </v-col>
+          </v-row>
           <v-row align="center">
             <v-col>
               <v-btn color="success" type="submit"> Submit </v-btn>
@@ -29,16 +46,17 @@ export default {
   data () {
     return {
       name: '',
-      container: []
+      containers: [{ image:'', name:'' }]
     }
   },
   methods: {
     async post_pod (e) {
       e.preventDefault()
-      let response = await backend.post(`/pods/${this.namespace}/`, {
-        name: this.name,
-        replicas: this.replicas
-      })
+      // let response = await backend.post(`/pods/${this.namespace}/`, {
+      //   name: this.name,
+      //   replicas: this.replicas
+      // })
+      console.log('POD MODAL DATA', this.name, this.containers)
       this.emit_event()
     },
     emit_event () {
@@ -50,7 +68,7 @@ export default {
 
 <style scoped>
 .modal {
-  width: 20vw;
+  width: 50vw;
   /* background-color: var(--v-primary) !important; */
 }
 </style>
